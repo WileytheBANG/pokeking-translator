fe391abc9aaec06d710de325a084c2579efc00dc
Wiley Nguyen <nguyenthethien111@gmail.com>
2025-05-25 06:33:46 +1000

    Add files via upload

--- background.js
+++ background.js
@@ -1,20 +1,5 @@
-const statePerTab = {};
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab || !tab.id) return;
 
-chrome.action.onClicked.addListener(async (tab) => {
-  const tabId = tab.id;
-
-  // Toggle the state for this tab
-  statePerTab[tabId] = !statePerTab[tabId];
-
-  // Send message to content script
-  chrome.tabs.sendMessage(tabId, { type: "TOGGLE_TRANSLATION" });
-
-  // Change icon
-  const icon = statePerTab[tabId] ? "icons/on.png" : "icons/off.png";
-  chrome.action.setIcon({ tabId, path: icon });
-});
-
-// Optional: reset icon when tab is closed or reloaded
-chrome.tabs.onRemoved.addListener((tabId) => {
-  delete statePerTab[tabId];
+  chrome.tabs.sendMessage(tab.id, { type: "toggle" });
 });
